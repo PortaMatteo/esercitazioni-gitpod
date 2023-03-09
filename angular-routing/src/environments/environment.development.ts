f2dc093d4cd8d59f58de5d5fd3d3c600f815bf5e
PortaMatteo <porta.matteo@iisgalvanimi.edu.it>
2023-03-09 13:58:25 +0000

    .

--- angular-routing/src/environments/environment.development.ts
+++ angular-routing/src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    oauthToken : "Bearer BQDUJqc6rD9VnTzxnT2SEUTCEqz0f_Q7Ht6AbNQt8Yq0DDSw3Mtkc32QRHhEb1R0Oewnjs2_LSrMh9VuhC5E7dgDRa1XdyJKX1A6JVNWlMkrWQw7QXFbx-t1q4smhaqU4KvZ9ST-tSYuMYvG43pPrR1obPZ96awLT3WdOj6IAjsmS5f3A-K5VuXmdnBIQZD3"
+    oauthToken : "Bearer BQCPR7sVHknzMpUZ2DIMAO0EFla-e1FpUwGAzCrQsO7jEfndCYcv3GYtNuABRAocKoHoUXGcE6BvYhAt6GcTqslwRQwmDAxoLje54PtsIlupo3SIDg2-O2sPekYMxGBbUciB5vduWnnu1xvFDxj19VwG5ssPAtY8By-_AXFpX1lNz7FYLiSR6HNVveYCiiJB"
 };
