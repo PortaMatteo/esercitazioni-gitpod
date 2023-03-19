34f0ea2830d6eb5c3948b386fe619d0187f1c809
PortaMatteo <porta.matteo@iisgalvanimi.edu.it>
2023-03-19 18:17:55 +0000

    .

--- cucina/src/app/cucina.service.ts
+++ cucina/src/app/cucina.service.ts
@@ -5,11 +5,10 @@ import {HttpClient} from '@angular/common/http';
   providedIn: 'root'
 })
 export class CucinaService {
-
   constructor(private http: HttpClient) { }
 
   searchProducts(query: string){
-    const url = `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${query}&page_size=2&json=true`
+    const url = `https://world.openfoodfacts.org/cgi/search.pl?search_terms=${query}&page_size=30&json=true`
     
     return this.http.get(url)
     
