03a88937ca9a2ba6f8e99dc78f5094d057f55eec
PortaMatteo <porta.matteo@iisgalvanimi.edu.it>
2023-05-07 20:37:42 +0000

    .

--- Geojson-leaflet/src/app/app.component.ts
+++ Geojson-leaflet/src/app/app.component.ts
@@ -10,6 +10,8 @@ import { GEOJSON, GeoFeatureCollection } from './models/geojson.model';
 export class AppComponent {
   private map: any;
   geoJsonObject !: any;
+  fillColor: any;
+
   
   private initMap(): void {
     // Creazione della mappa 
@@ -17,7 +19,7 @@ export class AppComponent {
       center: [45.464211, 9.191383], // Latitudine e longitudine del centro della mappa
       zoom: 12,
     });
-    
+
     // Aggiunta del tile alla mappa 
     const tiles = L.tileLayer( 
        // Aggiunge il Layer Tile che in questo caso prendiamo da openstreetmap 
@@ -28,17 +30,36 @@ export class AppComponent {
         attribution:
           '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       }
+
+      
     );  
 
     tiles.addTo(this.map);  // Aggiunge il tile alla mappa 
-    this.geoJsonObject = GEOJSON; 
+    this.geoJsonObject = GEOJSON;
 
-    L.geoJSON(this.geoJsonObject).addTo(this.map);
+    L.geoJSON(this.geoJsonObject).setStyle(this.styleFunc).addTo(this.map);
+    console.log(this.geoJsonObject);
+
+    for (var i = 0; i <= this.geoJsonObject.features.length; i++) {
+      L.marker([this.geoJsonObject.features[i].geometry.coordinates[0][0][1], this.geoJsonObject.features[i].geometry.coordinates[0][0][0]]).bindPopup(String(i)).addTo(this.map);
+    }
+  }
+  styleFunc = (feature: any) =>{
+    console.log(feature.properties.id)
+    let newColor = "#FF0000"; //RED
+    if(feature.properties.id == 0) newColor = "#00FF00"; //GREEN
+    else newColor = "#0000FF"; //BLUE
+    return ({
+        //clickable: false,
+        //fillColor: newColor,
+        //strokeWeight: 1
+        color: newColor
+    });
   }
 
   constructor() {
   }
-
+  
   ngAfterViewInit(): void {
     this.initMap();
   }
