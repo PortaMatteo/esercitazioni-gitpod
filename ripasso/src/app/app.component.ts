c6e74cfa99ff54c82b0862a212b6a24ca20b236f
PortaMatteo <porta.matteo@iisgalvanimi.edu.it>
2023-10-24 09:48:36 +0000

    .

---
+++ ripasso/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TypeList } from './type.model';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'ripasso';
+  obs: Observable <TypeList[]> = undefined!;
+  data: TypeList[]  = undefined!;
+  constructor(private http : HttpClient){
+    this.obs = this.http.get<TypeList[]>("https://pokeapi.co/api/v2/type")
+    this.obs.subscribe(this.dosomething)
+  }
+  dosomething = (data : TypeList[]) => {
+    this.data = data;
+  }
+}
---
+++ ripasso/src/app/type.model.ts
@@ -0,0 +1,11 @@
+export interface TypeList {
+    count: number
+    next: any
+    previous: any
+    results: Type[]
+  }
+  
+  export interface Type {
+    name: string
+    url: string
+  }
