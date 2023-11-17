5e967e69ad74bdeea2fd14c94fc56b9cab552cf0
PortaMatteo <porta.matteo@iisgalvanimi.edu.it>
2023-11-17 11:59:30 +0000

    .

--- Esplorazione-di-API-Rest/src/app/football.service.ts
+++ Esplorazione-di-API-Rest/src/app/football.service.ts
@@ -9,9 +9,9 @@ export class FootballService {
   constructor(private http: HttpClient) { }
 
   searchLeague(query: string) {
-    const url = `https://v3.football.api-sports.io/leagues?name=serie%20A`;
+    const url = `https://v3.football.api-sports.io/leagues?name=${query}`;
     const headers = new HttpHeaders({
-      "x-rapidapi-key": "",
+      "x-rapidapi-key": "57ea81c74e11d571e67dbe4d3f2ebd51",
       "x-rapidapi-host": "v3.football.api-sports.io"
     });
 
